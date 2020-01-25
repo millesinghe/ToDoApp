e80bee44b645f5c31b835f45440e31689b1c7e16
Milinda Bandara <dev.milinda@gmail.com>
2020-01-24 20:12:55 -0800

    add ToDo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { ErrorComponent } from './error/error.component';
+import { TodosComponent } from './todos/todos.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'welcome/:name', component: WelcomeComponent },
+  { path:'todo', component : TodosComponent},
   { path: '**', component: ErrorComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent } from './login/login.component';
 import { ErrorComponent } from './error/error.component';
+import { TodosComponent } from './todos/todos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
     LoginComponent,
-    ErrorComponent
+    ErrorComponent,
+    TodosComponent
   ],
   imports: [
     BrowserModule,
